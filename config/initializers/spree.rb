fff69bf4aa4846cebbce8501c6935acaa2985586
Prashant Patil <prashantpatil@Prashants-MacBook-Air.local>
2017-11-20 22:47:51 +0530

    fixxed issues

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -13,7 +13,6 @@ Spree.config do |config|
   # Example:
   # Uncomment to stop tracking inventory levels in the application
   # config.track_inventory_levels = false
-  config.admin_interface_logo = "logo.png"
 end
 
 Spree.user_class = "Spree::User"
